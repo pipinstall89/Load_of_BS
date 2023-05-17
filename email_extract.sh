7e74b326e679812f0eef9052126695880d0d609c
pipinstall89 <59775828+pipinstall89@users.noreply.github.com>
2023-05-17 09:23:19 -0400

    Create email_extract.sh
    bash script to organize windapsearch --users output

---
+++ email_extract.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Bash script for organizing a user list from windapsearch output
+# Sample windapsearch --users output:
+# cn: Doe, John
+# userPrincipalName: john.doe@company.com
+
+file_path="/path/to/user_list.txt"
+
+while IFS= read -r line
+do
+    if [[ $line == userPrincipalName:* ]]; then
+        email="${line#userPrincipalName: }"
+        echo "$email"
+    fi
+done < "$file_path"
